e443e95674ad174d6298e4025e5b43f413ea3a53
vlnguyen92 <nguyenvanlinh1992@gmail.com>
2015-10-07 11:14:33 -0400

    Added orginial files

---
+++ original/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <tchar.h>
+#include <malloc.h>
+
+#include <string.h>
+#include <cutil.h>
+
